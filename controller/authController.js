64f3e5bb42781f4afd7c1bf03ccc8818c03572f8
prastavana <prastavanahere@gmail.com>
2025-02-19 12:25:40 +0545

    Profile fetch codes

--- controller/authController.js
+++ controller/authController.js
@@ -175,16 +175,16 @@ const resetPassword = async (req, res) => {
     }
 };
 
-// Fetch user profile
+//Fetch Profile
 const getUserProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select("-password");
+        const user = await User.findById(req.user.id).select('-password'); // Exclude password
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(404).json({ message: 'User not found' });
         }
         res.json(user);
     } catch (error) {
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, forgotPassword, resetPassword, sendResetPasswordMail} = require('../controller/authController');
+const { registerUser, loginUser, forgotPassword, resetPassword, sendResetPasswordMail, getUserProfile} = require('../controller/authController');
 const {reset} = require("nodemon");
+const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/forgotPassword', forgotPassword);
 router.post('/sendResetMailPassword', sendResetPasswordMail);
 router.post('/reset-password', resetPassword);
-
+router.get('/profile', verifyToken, getUserProfile);
 
 
 module.exports = router;
